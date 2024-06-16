65521615eb52275525776f2a4703e645b319311a
Hiroaki Ono <hiono@student.42adel.org.au>
2024-06-16 18:17:48 +0930

    fixed main.cpp

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hiono <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 12:12:57 by hiono             #+#    #+#             */
-/*   Updated: 2024/06/07 19:08:43 by hiono            ###   ########.fr       */
+/*   Updated: 2024/06/16 18:16:23 by hiono            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,7 @@ int	main(void)
 
 	Zombie	*zombies = zombieHorde(number, name);
 	for (int i = 0; i < number; i++)
-	{
-		zombies[i] = Zombie(name);
 		zombies[i].announce();
-	}
 
 	std::cout << "\n";
 	std::cout << "--- delete all zombies ---" << "\n";
